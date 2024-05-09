4565311a60e1135ec897696cfd9443f85bda29d6
ryanma3003 <ryan.martin3003@gmail.com>
2024-05-10 00:34:34 +0700

    featured: add user and password

--- api/server.go
+++ api/server.go
@@ -22,6 +22,8 @@ func NewServer(store db.Store) *Server {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	router.POST("/users", server.createUser)
+
 	router.GET("/accounts", server.listAccount)
 	router.POST("/accounts", server.createAccount)
 	router.GET("/account/:id", server.getAccount)
--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) Users {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
